6cb3fc31a49db41d0bc783123886c34b99f5cb80
Fadhil <fadhilanugrah21@gmail.com>
2023-09-22 22:16:35 +0700

    add user model

--- BackEnd/server.ts
+++ BackEnd/server.ts
@@ -3,6 +3,6 @@ import connectDB from "./src/utils/db";
 require("dotenv").config();
 
 app.listen(process.env.PORT, () => {
-    console.log(`server is listening on ${process.env.PORT}`);
-    connectDB();
-})
+  console.log(`server is listening on ${process.env.PORT}`);
+  connectDB();
+});
