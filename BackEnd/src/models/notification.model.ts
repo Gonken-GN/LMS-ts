9c8ea8b0e6e232b9c76948a27a46ff1dc32fb0c8
Fadhil <fadhilanugrah21@gmail.com>
2023-10-28 12:39:06 +0700

    add some model

---
+++ BackEnd/src/models/notification.model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Model, Schema, mongo } from "mongoose";
+
+export interface INotification extends Document {
+  title: string;
+  message: string;
+  status: string;
+  userId: string;
+}
+
+const notificationSchema = new Schema<INotification>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "unread",
+    },
+  },
+  { timestamps: true }
+);
+
+const notificationiModel: Model<INotification> = mongoose.model("Notifications", notificationSchema);
+
+export default notificationiModel;
